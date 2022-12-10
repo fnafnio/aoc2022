6d06a64f63f52318e70bb1a30059120886a7a6f7
Fabio Pungg <fabiopungg@gmail.com>
2022-12-10 20:53:49 +0100

    scaffolding

---
+++ src/solutions/day_8.rs
@@ -0,0 +1,13 @@
+use crate::Solver;
+
+pub struct Day8;
+
+impl Solver for Day8 {
+    fn part_1(&self, input: &str) -> String {
+        todo!()
+    }
+
+    fn part_2(&self, input: &str) -> String {
+        todo!()
+    }
+}
--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -5,6 +5,7 @@ mod day_4;
 mod day_5;
 mod day_6;
 mod day_7;
+mod day_8;
 
 pub use day_1::Day1;
 pub use day_2::Day2;
@@ -13,3 +14,4 @@ pub use day_4::Day4;
 pub use day_5::Day5;
 pub use day_6::Day6;
 pub use day_7::Day7;
+pub use day_8::Day8;
