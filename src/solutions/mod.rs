4ebef97f94d38e8669c4bf44d72d46312dd82f3a
F_Punk <fabiopungg@gmail.com>
2022-12-10 20:39:04 +0100

    Day_7 (#2)

--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -4,6 +4,7 @@ mod day_3;
 mod day_4;
 mod day_5;
 mod day_6;
+mod day_7;
 
 pub use day_1::Day1;
 pub use day_2::Day2;
@@ -11,3 +12,4 @@ pub use day_3::Day3;
 pub use day_4::Day4;
 pub use day_5::Day5;
 pub use day_6::Day6;
+pub use day_7::Day7;
