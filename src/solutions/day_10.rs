dbbfba430f7f7d209ecca8dff35830ece6d44309
Fabio Pungg <fabiopungg@gmail.com>
2022-12-13 10:52:36 +0100

    parser works

--- src/solutions/day_10.rs
+++ src/solutions/day_10.rs
@@ -11,3 +11,52 @@ impl Solver for Day {
         todo!()
     }
 }
+
+struct Cpu {
+    cycle: usize,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+enum Instruction {
+    Noop,
+    Addx(isize),
+}
+
+    use nom::{
+        branch::alt,
+        bytes::complete::tag,
+        character::complete::multispace1,
+        combinator::{self, map},
+        sequence::{pair, preceded, separated_pair},
+        IResult,
+    };
+
+
+    fn parse_isize(i: &str) -> IResult<&str, isize> {
+        combinator::map(nom::character::complete::i64, |n| n as _)(i)
+    }
+
+    fn command(i: &str) -> IResult<&str, Instruction> {
+        let noop = map(tag("noop"), |_| Instruction::Noop);
+        let addx = map(preceded(pair(tag("addx"), multispace1), parse_isize), |i| {
+            Instruction::Addx(i)
+        });
+        alt((noop, addx))(i)
+        // Ok((i, Instruction::Noop))
+    }
+
+#[cfg(test)]
+mod tests {
+
+    use test_case::test_case;
+    use super::*;
+
+    #[test_case("noop" => Instruction::Noop)]
+    #[test_case("addx 3" => Instruction::Addx(3))]
+    #[test_case("addx -5" => Instruction::Addx(-5))]
+    fn test_parser(input: &str) -> Instruction {
+        command(input).unwrap().1
+    }
+
+
+}
