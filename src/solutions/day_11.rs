71ef57c55e54c6fcef36a76765aa382355a99647
F_Punk <fabiopungg@gmail.com>
2022-12-13 22:25:29 +0100

    finished the parsers

--- src/solutions/day_11.rs
+++ src/solutions/day_11.rs
@@ -1,8 +1,13 @@
+use std::{collections::VecDeque, str::FromStr};
+
 use nom::{
+    branch::alt,
     bytes::complete::tag,
+    character::{complete::one_of, streaming::multispace0},
     combinator,
     multi::{many1, separated_list0, separated_list1},
     number::complete,
+    sequence::{preceded, tuple},
     IResult,
 };
 
@@ -20,17 +25,114 @@ impl Solver for Day {
     }
 }
 
-fn parse_number(i: &str) -> IResult<&str, isize> {
-    combinator::map(nom::character::complete::i64, |n| n as _)(i)
+fn starting_items(i: &str) -> IResult<&str, VecDeque<u32>> {
+    preceded(tag("Starting items: "), item_list)(i)
 }
 
-fn starting_items(i: &str) -> IResult<&str, Vec<usize>> {
-    let number_list = separated_list0(
-        tag(", "),
-        combinator::map(complete::u64, |v| v::parse().unwrap()),
-    );
+fn item_list(i: &str) -> IResult<&str, VecDeque<u32>> {
+    let number_list = separated_list0(tag(", "), nom::character::complete::u32);
+    combinator::map(number_list, |l| l.into())(i)
+}
 
-    number_list(i)
+#[derive(Debug, PartialEq)]
+enum Operation {
+    Mul(Term, Term),
+    Add(Term, Term),
+}
+
+fn parse_monkey(i: &str) -> IResult<&str, usize> {
+    preceded(
+        tag("Monkey: "),
+        combinator::map(nom::character::complete::u32, |u| u as usize),
+    )(i)
+}
+
+impl Operation {
+    fn parse(i: &str) -> IResult<&str, Self> {
+        let (i, _) = tag("Operation: new = ")(i)?;
+        let (i, (t1, op, t2)) = tuple((Term::parse, one_of("+*"), Term::parse))(i)?;
+        match op {
+            '+' => Ok((i, Self::Add(t1, t2))),
+            '*' => Ok((i, Self::Mul(t1, t2))),
+            _ => {
+                unreachable!()
+            }
+        }
+    }
+}
+
+// impl FromStr for Operation {
+//     type Err;
+
+//     fn from_str(s: &str) -> Result<Self, Self::Err> {
+//         todo!()
+//     }
+// }
+
+#[derive(Debug, PartialEq)]
+enum Term {
+    Old,
+    Val(u32),
+}
+
+impl Term {
+    fn parse(i: &str) -> IResult<&str, Self> {
+        alt((
+            combinator::map(tag("old"), |_| Self::Old),
+            combinator::map(nom::character::complete::u32, |u| Self::Val(u)),
+        ))(i)
+    }
+}
+
+fn parse_usize(i: &str) -> IResult<&str, usize> {
+    combinator::map(nom::character::complete::u32, |u| u as usize)(i)
+}
+
+fn parse_divisor(i: &str) -> IResult<&str, u32> {
+    preceded(tag("Test: divisible by "), nom::character::complete::u32)(i)
+}
+
+fn parse_true(i: &str) -> IResult<&str, usize> {
+    preceded(
+        multispace0,
+        preceded(tag("If true: throw to monkey "), parse_usize),
+    )(i)
+}
+fn parse_false(i: &str) -> IResult<&str, usize> {
+    preceded(
+        multispace0,
+        preceded(tag("If false: throw to monkey "), parse_usize),
+    )(i)
+}
+
+#[derive(Debug, PartialEq)]
+struct Monkey {
+    items: VecDeque<u32>,
+    op: Operation,
+    test: u32,
+    yes: usize,
+    no: usize,
+}
+
+impl Monkey {
+    fn parse(i: &str) -> IResult<&str, Self> {
+        let (i, number) = parse_monkey(i)?;
+        let (i, items) = starting_items(i)?;
+        let (i, op) = Operation::parse(i)?;
+        let (i, test) = parse_divisor(i)?;
+        let (i, yes) = parse_true(i)?;
+        let (i, no) = parse_false(i)?;
+        Ok((
+            i,
+            Self {
+                items,
+                op,
+                test,
+                yes,
+                no,
+            },
+        ))
+    }
 }
 
 #[cfg(test)]
