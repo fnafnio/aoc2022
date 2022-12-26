e22c55bca07e7a08c38755ecd9dbbe71204250db
Fabio Pungg <fabiopungg@gmail.com>
2022-12-26 18:57:57 +0100

    changed to isize

--- day_14/src/main.rs
+++ day_14/src/main.rs
@@ -16,24 +16,24 @@ use nom::sequence::separated_pair;
 use nom::{self, IResult};
 #[derive(Clone, Copy, Debug, Add, AddAssign, Sub, SubAssign, PartialEq, Eq, Hash)]
 struct Vec2 {
-    x: usize,
-    y: usize,
+    x: isize,
+    y: isize,
 }
 
 impl Vec2 {
-    fn as_index(&self, (width, height): (usize, usize)) -> usize {
+    fn as_index(&self, (width, height): (isize, isize)) -> usize {
         (self.y * width + self.x) as usize
     }
 
     fn from_index(index: usize, (width, height): (usize, usize)) -> Self {
-        let x = index.rem_euclid(width) as usize;
-        let y = index.div_floor(width) as usize;
+        let x = index.rem_euclid(width) as isize;
+        let y = index.div_floor(width) as isize;
         (x, y).into()
     }
 
     fn parse(input: &str) -> IResult<&str, Self> {
         map(
-            separated_pair(complete_usize, tag(","), complete_usize),
+            separated_pair(complete_isize, tag(","), complete_isize),
             |(x, y)| Self { x, y },
         )(input)
     }
@@ -47,8 +47,18 @@ fn complete_usize(input: &str) -> IResult<&str, usize> {
     map(nom::character::complete::u64, |u| u as usize)(input)
 }
 
+fn complete_isize(input: &str) -> IResult<&str, isize> {
+    map(nom::character::complete::i64, |u| u as isize)(input)
+}
+
 impl From<(usize, usize)> for Vec2 {
     fn from((x, y): (usize, usize)) -> Self {
+        let (x, y) = (x as isize, y as isize);
+        Self { x, y }
+    }
+}
+impl From<(isize, isize)> for Vec2 {
+    fn from((x, y): (isize, isize)) -> Self {
         Self { x, y }
     }
 }
